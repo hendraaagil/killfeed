d32d7630dcce559422be7ac3c1f65271319585c2
hendraaagil <hendraaagil@gmail.com>
2026-07-30 12:33:04 +0700

    chore: setup project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex min-h-svh items-center justify-center">
+      <h1 className="text-3xl font-bold">Val Kill Feed Generator</h1>
+    </main>
+  )
+}
+
+export default App
